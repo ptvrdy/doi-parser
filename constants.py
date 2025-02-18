4401f689c9b8926224824e58d7dc3d9853dd90eb
ptvrdy <peyton.tvrdy123@gmail.com>
2025-02-18 15:32:18 -0500

    New Collection BTS Data Spotlight

--- constants.py
+++ constants.py
@@ -25,6 +25,7 @@ collections_to_doi_lookup = {
     "Advisory Circulars": "https://doi.org/10.21949/1530859", 
     "Air Quality + Sustainable Transportation Highlights": "https://doi.org/10.21949/y3r3-3y74",
     "Attitudes and Behavior Surveys": "https://doi.org/10.21949/45f6-zs20",
+    "BTS Data Spotlight": "https://doi.org/10.21949/e6w2-7784",
     "BTS Ports": "https://doi.org/10.21949/1530821",
     "BTS Products": "https://doi.org/10.21949/1530822",
     "CAA and FAA Reports (CAAFAA)": "https://doi.org/10.21949/1530064",
